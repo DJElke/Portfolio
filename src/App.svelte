365213c274634c11f1916b47e844351a3a6f3f4c
elkedejonckere <elkedejonckere@gmail.com>
2020-11-05 16:07:13 +0100

    mainpage setup + css

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,29 @@
 <script>
-	
+	import Header from './LayoutComponents/Header.svelte';
 </script>
 
 <main>
-	<h1><span class="h1Thin">Under</span>    Construction</h1>
-	<div class="homeImageWrapper">
-	</div>
+	<Header/>
+	<!-- <img class="blobImg" src="/images/homepagename.svg" alt="name"> -->
 </main>
 
 <style>
 	main {
+		height: 100%;
+		position: relative;
+        background-image: url("/images/Home.svg");
+        background-size:100% 100%;
+		background-repeat: no-repeat;
+	}
 
-	}
-	
-	h1{
-		z-index: 1;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		font-family: Condor;
-		color: white;
-		font-size: 40pt;
-	}
-	.h1Thin{
-		font-weight: lighter;
+	.blobImg{
+		display: block;
+		margin: auto;
 	}
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
-	}
-
-	.homeImageWrapper{
-		z-index:-1;
-		width: auto;
-        height: 640px;
-        max-height: 100%;
-        margin: 0;
-        padding: 0;
-        background-image: url("/images/portfolio-face-01.svg");
-        background-size:100% 100%;
-        background-repeat: no-repeat;
-		object-fit: cover;
 	}
 </style>
---
+++ src/LayoutComponents/Header.svelte
@@ -0,0 +1,44 @@
+<header>
+    <nav>
+        <ul class="navigation">
+            <li class="navigationObj navigationObj--home"><a href="#">EDJ</a></li>
+            <li class="navigationObj"><a href="#">Contact</a></li>
+            <li class="navigationObj"><a href="#">Projects</a></li>
+            <li class="navigationObj"><a href="#">Skills</a></li>
+            <li class="navigationObj"><a href="#">About</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    .navigation{
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        height: 5rem;
+        overflow: hidden;
+        font-family: Condor;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 5rem;
+    }
+
+    .navigationObj{
+        float: right;
+        vertical-align: middle;
+    }
+
+    .navigationObj a{
+        display: block;
+        text-align: center;
+        padding-left: 5rem;
+        padding-right: 5rem;
+        text-decoration: none;
+        color: #EC5F33;
+    }
+
+    .navigationObj--home{
+        float: left;
+        font-size: 36px;
+    }
+</style>
